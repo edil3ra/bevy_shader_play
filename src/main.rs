dd462d443cf5f425f0f701cae35f8beb39bdec67
edil3ra <edil3ra@gmail.com>
2025-05-08 14:05:21 +0200

    a

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// Support configuring Bevy lints within code.
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+
+use bevy::prelude::*;
+use bevy_new_2d::AppPlugin;
+
+fn main() -> AppExit {
+    
+}
